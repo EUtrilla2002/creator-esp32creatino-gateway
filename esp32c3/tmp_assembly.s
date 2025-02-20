3e0c85ee6eb2d9468d39748d7d4f2c6a351ab472
EUtrilla2002 <100451242@alumnos.uc3m.es>
2025-02-20 13:56:50 +0100

    Implementando ecall en creatino

--- esp32c3/tmp_assembly.s
+++ esp32c3/tmp_assembly.s
@@ -1,12 +1,33 @@
+####################################################
+# CREATINO EXAMPLE: Hello world                    #
+#               BY ELISA UTRILLA                   #
+####################################################      
 
-#
-# Creator (https://creatorsim.github.io/creator/)
-#
-
+#ARDUINO
 .data
-  w3: .word 1, 2, 3, 4, 5
+    msg: .string "Hola %s\n"
+    arg: .string "Mundo"
+
 
 .text
+setup:
+    li a0,115200 
+    addi sp, sp, -4      
+    sw ra, 0(sp)     # Guardar el valor de ra (return address)
+    jal ra,cr_serial_begin
+    lw ra, 0(sp)     # Recupera el valor de ra
+    addi sp, sp,4 
+
+    la a0, msg
+    la a1, arg
+    addi sp, sp, -16       # Reservar espacio en el stack
+    sw ra, 12(sp)          # Guardar el registro RA en el stack
+    jal ra,cr_serial_printf
+    lw ra, 12(sp)          # Restaurar el registro RA desde el stack
+    addi sp, sp, 16       # Liberar el espacio del stack
+    jr ra
+loop:
+    nop
 main:
 
   li  t3,  1
