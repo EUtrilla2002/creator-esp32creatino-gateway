df8abac9da994e8fab67bab745e13c2f22589bea
EUtrilla2002 <100451242@alumnos.uc3m.es>
2025-02-25 12:13:48 +0100

    Añadido monitor al gdbgui

--- esp32c3/tmp_assembly.s
+++ esp32c3/tmp_assembly.s
@@ -3,6 +3,21 @@
 # Creator (https://creatorsim.github.io/creator/)
 #
 
+.data
+ 
+   byte:       .byte    12
+    
+   .align 1
+   half:       .half    34
+    
+   .align 2
+   word:       .word   -5678
+   float:      .float   456.322
+   double:     .double  9741.34
+   stringz:    .string  "This is a string"
+   string:     .string  "This is another string"
+   space:      .zero 32
+   
 .text
 
 main:
