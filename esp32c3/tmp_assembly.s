a531bda2eb45e65c29975cef9dd3ac1edbaad949
EUtrilla2002 <100451242@alumnos.uc3m.es>
2025-04-23 10:32:06 +0200

    Añadida función erase-flash

--- esp32c3/tmp_assembly.s
+++ esp32c3/tmp_assembly.s
@@ -1,55 +1,45 @@
 
-#
-# ARCOS.INF.UC3M.ES
-# BY-NC-SA (https://creativecommons.org/licenses/by-nc-sa/4.0/deed.es)
-#
-
-
+.data
+    delay:
+        .word 1000
 .text
-
-     main: 
-           addi sp, sp, -4
-           sw ra, 0(sp)
-
-           # t1 = factorial(5)
-           li  a0, 5
-           jal x1, factorial
-
-           # print_int(t1)
-           li  a7, 1
-           ecall
-
-           # return
-           lw ra, 0(sp)
-           addi sp, sp, 4
-           jr ra
-
-
-factorial:
-           # crear "stack frame" para $ra, $fp y una variable local
-           addi sp, sp, -12
-           sw   ra, 8(sp)
-           sw   fp, 4(sp)
-           addi fp, sp, 4
-
-           # if (a0 < 2):
-           #     return 1
-           li   x5, 2
-           bge  a0, t0, b_else
-           li   a0, 1 
-           beq  x0, x0, b_efs
-           # else:
-           #    return a0 * factorial(a0 - 1)
-   b_else: sw   a0, -4(fp)
-           addi a0, a0, -1
-           jal  x1, factorial
-           lw   t1, -4(fp)
-           mul  a0, a0, t1
-
-           # finalizar "stack frame"
-   b_efs:  lw   ra, 8(sp)
-           lw   fp, 4(sp)
-           addi sp, sp, 12
-
-           # return t0
-           jr ra
+setup:
+    #pinMode(LED_BUILTIN, OUTPUT);
+    li a0,30
+    li a1,  0x03 
+    jal ra, cr_pinMode
+loop:
+    #digitalWrite(LED_BUILTIN, HIGH);
+    li a0,30 
+    li a1, 0x1
+    jal ra, cr_digitalWrite
+    #delay(1000);
+    la a0, delay
+    lw a0, 0(a0)
+    addi sp, sp, -16      
+    sw ra, 12(sp)
+    jal ra, cr_delay
+    lw ra, 12(sp)          
+    addi sp, sp, 16 
+    #digitalWrite(LED_BUILTIN, LOW);
+    li a0,30 
+    li a1, 0x0
+    jal ra, cr_digitalWrite
+    #delay(1000);
+    la a0, delay
+    lw a0, 0(a0)
+    addi sp, sp, -16      
+    sw ra, 12(sp)
+    jal ra, cr_delay
+    lw ra, 12(sp)          
+    addi sp, sp, 16 
+    
+main:
+    addi sp, sp, -16       
+    sw ra, 12(sp)          
+    jal ra, cr_initArduino    
+    jal ra, setup
+    lw ra, 12(sp)          
+    addi sp, sp, 16              
+    li  t0, 1
+    beqz t0, loop       
